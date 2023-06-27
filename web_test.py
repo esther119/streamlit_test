89a345a657db55bed7419e2b28e55f447a42edbc
swimmingcircle119 <lampmaa22@gmail.com>
2023-06-27 16:02:44 -0700

    langchain test

--- web_test.py
+++ web_test.py
@@ -1,38 +1,64 @@
+"""Python file to serve as the frontend"""
 import streamlit as st
-import pandas as pd
-import numpy as np
+from streamlit_chat import message
+import pinecone
+import os
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
 
-st.title('My first app')
+from langchain.chains import ConversationChain
+from langchain.llms import OpenAI
+import pinecone_api
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+embeddings = OpenAIEmbeddings()
 
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-# data_load_state.text('Loading data...done!')
-data_load_state.text("Done! (using st.cache_data)")
-
-st.subheader('Raw data')
-st.write(data)
-
-st.subheader('Number of pickups by hour')
-
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-st.bar_chart(hist_values)
+pinecone_api_key = pinecone_api.pinecone_api_key
 
 
+pinecone.init(
+    api_key=pinecone_api_key,  
+    environment='asia-southeast1-gcp-free'   
+)
+index_name =  'tim-urban-test'
+
+
+
+def load_chain():
+    """Logic for loading the chain you want to use should go here."""
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    return docsearch
+
+chain = load_chain()
+
+# From here down is all the StreamLit UI.
+st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
+st.header("LangChain Demo")
+
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+
+
+def get_text():
+    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    return input_text
+
+
+user_input = get_text()
+
+if user_input:
+    if user_input:
+        docs = chain.similarity_search(user_input)
+        output = docs[0].page_content
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state["generated"]:
+
+    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
